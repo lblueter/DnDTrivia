906335758e65d94bf4f35244449c3a55d1591af6
lblueter <logan11023@yahoo.com>
2018-09-01 00:31:09 -0400

    building the basics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,47 @@
 var theMostImportantVariable = true;
 var moveLeftPlease = alert(theMostImportantVariable)
+
+var questionOne = {
+    content: "This is the first question",
+    answer: "correct",
+    trapOne: "wrong1",
+    trapTwo: "wrong2",
+    trapThree: "wrong3",
+}
+var questionTwo = {
+    content: "This is the second question",
+    answer: "correct",
+    trapOne: "wrong1",
+    trapTwo: "wrong2",
+    trapThree: "wrong3",
+}
+var questionThree = {
+    content: "This is the third question",
+    answer: "correct",
+    trapOne: "wrong1",
+    trapTwo: "wrong2",
+    trapThree: "wrong3",
+}
+var questionFour = {
+    content: "This is the fourth question",
+    answer: "correct",
+    trapOne: "wrong1",
+    trapTwo: "wrong2",
+    trapThree: "wrong3",
+}
+var questionFive = {
+    content: "This is the fifth question",
+    answer: "correct",
+    trapOne: "wrong1",
+    trapTwo: "wrong2",
+    trapThree: "wrong3",
+}
+var questionsAnswered = 0;
+var answeredCorrect = 0;
+var answeredWrong = 0;
+var time = 30;
+
+
+$("#start").on("click", function () {
+
+})
