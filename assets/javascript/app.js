3748f74330093dcedada1d9dd2a2e751a973241e
lblueter <logan11023@yahoo.com>
2018-09-01 17:19:15 -0400

    ultra combo, coding extravaganza and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,46 +2,198 @@ var theMostImportantVariable = true;
 var moveLeftPlease = alert(theMostImportantVariable)
 
 var questionOne = {
-    content: "This is the first question",
-    answer: "correct",
-    trapOne: "wrong1",
-    trapTwo: "wrong2",
-    trapThree: "wrong3",
+    content: "What year was Dungeons & Dragons created?",
+    answer: "1974",
+    trapOne: "1997",
+    trapTwo: "1984",
+    trapThree: "1970",
 }
 var questionTwo = {
-    content: "This is the second question",
-    answer: "correct",
-    trapOne: "wrong1",
-    trapTwo: "wrong2",
-    trapThree: "wrong3",
+    content: "What is the name of the creator of Dungeons & Dragons?",
+    answer: "Gary Gygax",
+    trapOne: "Matthew Mercer",
+    trapTwo: "Wizards of the Coast",
+    trapThree: "Tactical Studies Rules",
 }
 var questionThree = {
-    content: "This is the third question",
-    answer: "correct",
-    trapOne: "wrong1",
-    trapTwo: "wrong2",
-    trapThree: "wrong3",
+    content: "Which edition of Dungeons & Dragons first used THAC0?",
+    answer: "Advanced Dungeons & Dragons",
+    trapOne: "Dungeons & Dragons 3.5",
+    trapTwo: "Dungeons & Dragons 5e",
+    trapThree: "Original Dungeons & Dragons",
 }
 var questionFour = {
-    content: "This is the fourth question",
-    answer: "correct",
-    trapOne: "wrong1",
-    trapTwo: "wrong2",
-    trapThree: "wrong3",
+    content: "What is the name of the game that rivaled D&D 4th edition?",
+    answer: "Pathfinder",
+    trapOne: "Better Dungeons & Dragons",
+    trapTwo: "Mutants & Masterminds",
+    trapThree: "Shadowrun",
 }
 var questionFive = {
-    content: "This is the fifth question",
-    answer: "correct",
-    trapOne: "wrong1",
-    trapTwo: "wrong2",
-    trapThree: "wrong3",
+    content: "What is the name of the most legendary monster in Dungeons & Dragons?",
+    answer: "Tarrasque",
+    trapOne: "Beholder",
+    trapTwo: "Lich",
+    trapThree: "Mind Flayer",
 }
 var questionsAnswered = 0;
 var answeredCorrect = 0;
 var answeredWrong = 0;
 var time = 30;
-
-
+var intervalID
+var count = function () {
+    time--
+    $(".time").text("Time Remaining: " + time)
+    if (time <= 0) {
+        clearInterval(intervalID)
+        if (questionsAnswered === 0) {
+            return wrongAnswer(questionOne)
+        }
+        else if (questionsAnswered === 1) {
+            return wrongAnswer(questionTwo)
+        }
+        else if (questionsAnswered === 2) {
+            return wrongAnswer(questionThree)
+        }
+        else if (questionsAnswered === 3) {
+            return wrongAnswer(questionFour)
+        }
+        else if (questionsAnswered === 4) {
+            return wrongAnswer(questionFive)
+        }
+    }
+}
+var nextQuestion = function () {
+    $("#answerDiv").show()
+    $("#displayDiv").hide()
+    time = 30
+    intervalID = setInterval(count, 1000)
+    if (questionsAnswered === 1) {
+        $(".question").text(questionTwo.content)
+        $("#answerA").text(questionTwo.trapTwo)
+        $("#answerB").text(questionTwo.trapThree)
+        $("#answerC").text(questionTwo.trapOne)
+        $("#answerD").text(questionTwo.answer)
+    }
+    else if (questionsAnswered === 2) {
+        $(".question").text(questionThree.content)
+        $("#answerA").text(questionThree.trapThree)
+        $("#answerB").text(questionThree.trapTwo)
+        $("#answerC").text(questionThree.answer)
+        $("#answerD").text(questionThree.trapOne)
+    }
+    else if (questionsAnswered === 3) {
+        $(".question").text(questionFour.content)
+        $("#answerA").text(questionFour.answer)
+        $("#answerB").text(questionFour.trapTwo)
+        $("#answerC").text(questionFour.trapThree)
+        $("#answerD").text(questionFour.trapOne)
+    }
+    else if (questionsAnswered === 4) {
+        $(".question").text(questionFive.content)
+        $("#answerA").text(questionFive.trapOne)
+        $("#answerB").text(questionFive.trapTwo)
+        $("#answerC").text(questionFive.answer)
+        $("#answerD").text(questionFive.trapThree)
+    }
+}
+var correctAnswer = function () {
+    questionsAnswered++
+    $(".question").text("Correct!")
+    clearInterval(intervalID)
+    $("#answerDiv").hide()
+    setTimeout(nextQuestion, 3000)
+}
+var wrongAnswer = function (of) {
+    questionsAnswered++
+    if (time <= 0) {
+        $(".question").text("Time's Up!")
+    }
+    else {
+        $(".question").text("Wrong!")
+    }
+    clearInterval(intervalID)
+    $("#answerDiv").hide()
+    $("#displayDiv").show()
+    $("#rightWrong").text("The Correct Answer was: " + of.answer)
+    setTimeout(nextQuestion, 3000)
+}
 $("#start").on("click", function () {
-
+    $(".question").text(questionOne.content)
+    $("#answerA").text(questionOne.trapOne)
+    $("#answerB").text(questionOne.answer)
+    $("#answerC").text(questionOne.trapTwo)
+    $("#answerD").text(questionOne.trapThree)
+    $(".time").text("Time Remaining: " + time)
+    $("#start").hide()
+    intervalID = setInterval(count, 1000)
+})
+$("#answerA").on("click", function () {
+    if (questionsAnswered === 0) {
+        console.log("works")
+        return wrongAnswer(questionOne)
+    }
+    else if (questionsAnswered === 1) {
+        return wrongAnswer(questionTwo)
+    }
+    else if (questionsAnswered === 2) {
+        return wrongAnswer(questionThree)
+    }
+    else if (questionsAnswered === 3) {
+        return correctAnswer()
+    }
+    else if (questionsAnswered === 4) {
+        return wrongAnswer(questionFive)
+    }
+})
+$("#answerB").on("click", function () {
+    if (questionsAnswered === 0) {
+        return correctAnswer()
+    }
+    else if (questionsAnswered === 1) {
+        return wrongAnswer(questionTwo)
+    }
+    else if (questionsAnswered === 2) {
+        return wrongAnswer(questionThree)
+    }
+    else if (questionsAnswered === 3) {
+        return wrongAnswer(questionFour)
+    }
+    else if (questionsAnswered === 4) {
+        return wrongAnswer(questionFive)
+    }
+})
+$("#answerC").on("click", function () {
+    if (questionsAnswered === 0) {
+        return wrongAnswer(questionOne)
+    }
+    else if (questionsAnswered === 1) {
+        return wrongAnswer(questionTwo)
+    }
+    else if (questionsAnswered === 2) {
+        return correctAnswer()
+    }
+    else if (questionsAnswered === 3) {
+        return wrongAnswer(questionFour)
+    }
+    else if (questionsAnswered === 4) {
+        return correctAnswer()
+    }
+})
+$("#answerD").on("click", function () {
+    if (questionsAnswered === 0) {
+        return wrongAnswer(questionOne)
+    }
+    else if (questionsAnswered === 1) {
+        return correctAnswer()
+    }
+    else if (questionsAnswered === 2) {
+        return wrongAnswer(questionThree)
+    }
+    else if (questionsAnswered === 3) {
+        return wrongAnswer(questionFour)
+    }
+    else if (questionsAnswered === 4) {
+        return wrongAnswer(questionFive)
+    }
 })
